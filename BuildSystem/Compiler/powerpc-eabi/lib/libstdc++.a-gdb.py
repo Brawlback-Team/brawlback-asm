e39b4a2ae63572257d1ae97f7c246a46abc94e6a
Jared White <jwhite2292forward@gmail.com>
2022-04-07 15:17:09 -0500

    Update devkitppc version to gcc 11

--- BuildSystem/Compiler/powerpc-eabi/lib/libstdc++.a-gdb.py
+++ BuildSystem/Compiler/powerpc-eabi/lib/libstdc++.a-gdb.py
@@ -1,5 +1,5 @@
 # -*- python -*-
-# Copyright (C) 2009-2018 Free Software Foundation, Inc.
+# Copyright (C) 2009-2021 Free Software Foundation, Inc.
 
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -19,8 +19,8 @@ import gdb
 import os
 import os.path
 
-pythondir = '/opt/devkitpro/x86_64-w64-mingw32/devkitPPC/share/gcc-8.4.0/python'
-libdir = '/opt/devkitpro/x86_64-w64-mingw32/devkitPPC/powerpc-eabi/lib'
+pythondir = '/home/davem/projects/devkitpro/tool-packages/devkitPPC/src/build/x86_64-w64-mingw32/devkitPPC/share/gcc-11.2.0/python'
+libdir = '/home/davem/projects/devkitpro/tool-packages/devkitPPC/src/build/x86_64-w64-mingw32/devkitPPC/powerpc-eabi/lib'
 
 # This file might be loaded when there is no current objfile.  This
 # can happen if the user loads it manually.  In this case we don't
