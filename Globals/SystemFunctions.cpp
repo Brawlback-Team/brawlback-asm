e482368812d6cb9cc056bbb23e9ac5c82e585e4f
Elias Reid <elias.reid2@gmail.com>
2022-04-08 00:59:18 -0300

    string functions to SystemFunctions.cpp

--- Globals/SystemFunctions.cpp
+++ Globals/SystemFunctions.cpp
@@ -7,6 +7,20 @@
 #define system_memset ((void* (*)(void* data, int value, size_t size)) 0x8000443c)
 #define system_memcmp ((int (*)(const void* a, const void* b, size_t size)) 0x803f6150)
 
+#define system_strcat ((char* (*)(char* destination, const char* source)) 0x803fa384)
+#define system_strcpy ((char* (*)(char* destination, const char* source)) 0x803fa280)
+
+//used anywhere?
+#define system_strcpylen ((char* (*)(char* destination, const char* source, int size)) 0x803fa340)
+//used anywhere?
+#define system_sprintf ((int (*)(char* buffer, const char* format, ...)) 0x803f89fc)
+#define system_strstr ((char* (*)(const char* buffer, const char* target)) 0x803fa798)
+#define system_strcmp ((int (*)(const char* str1, const char* str2)) 0x803fa3fc)
+
+#define system_atoi ((int (*)(const char* buffer)) 0x803fc704)
+#define system_atof ((float (*)(const char* buffer)) 0x803fbbf8)
+
+
 extern "C" void* memmove(void* dest, const void* source, size_t size) {
 	return system_memmove(dest, source, size);
 }
@@ -31,3 +45,28 @@ extern "C" void* malloc(size_t size) {
 extern "C" void free(void* ptr) {
 	freeToExpHeap(mainHeap, ptr);
 }
+
+extern "C" char* strcat(char* destination, const char* source){
+	return system_strcat(destination, source);
+}
+
+extern "C" char* strcpy(char* destination, const char* source){
+	return system_strcpy(destination, source);
+}
+
+extern "C" char* strstr(const char* buffer, const char* target){
+	return system_strstr(buffer, target);
+}
+
+extern "C" int atoi( const char* buffer){
+	return system_atoi( buffer );
+}
+
+extern "C" float atof( const char* buffer){
+	return system_atof( buffer );
+}
+
+//not sure how to pass var number of arguments through to system_sprintf
+// extern "C" int sprintf( char* buffer, const char* format, ... ){
+// 	return system_sprintf( buffer, format, ... );
+// }
