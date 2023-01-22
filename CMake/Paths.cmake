bcb12b8c564e1eed1875c1a02e3404317b1013bb
Elias Reid <7339667+eliasreid@users.noreply.github.com>
2023-01-22 13:39:43 -0400

    use toolchain file to specifiy compiler, simplify various cmake things

--- CMake/Paths.cmake
+++
@@ -1,9 +0,0 @@
-set(buildSystemDirectory ${PROJECT_SOURCE_DIR}/BuildSystem)
-set(builder ${buildSystemDirectory}/src/Build.py)
-set(PPC_BIN_DIRECTORY "${buildSystemDirectory}/Compiler/bin")
-set(functionMapDirectory "${buildSystemDirectory}/BrawlFuncMap.map")
-
-set(librariesDirectory ${PROJECT_SOURCE_DIR}/Libraries)
-set(globalsDirectory ${PROJECT_SOURCE_DIR}/Globals)
-set(codesDirectory ${PROJECT_SOURCE_DIR}/Codes)
-set(sdCardDirectory ${PROJECT_SOURCE_DIR}/SDCard)
---
+++ toolchain.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR ppc)
+set(CMAKE_CROSSCOMPILING 1)
+
+#TODO: rename buildSystem directory, sounds too much like SRC_DIR/BuildSystem, which it is not
+# equal to on Linux
+if(WIN32)
+  set(buildSystemDirectory ${CMAKE_CURRENT_LIST_DIR}/BuildSystem)
+else()
+  set(buildSystemDirectory $ENV{DEVKITPPC})
+endif()
+
+set(CMAKE_CXX_COMPILER ${buildSystemDirectory}/bin/powerpc-eabi-g++)
+
+set(CMAKE_CXX_STANDARD 17)
