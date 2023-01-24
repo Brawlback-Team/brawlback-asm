c9d6bc4fb12a3c3fb4a314626b4837bb39a5db01
Elias Reid <47339667+eliasreid@users.noreply.github.com>
2023-01-23 23:26:06 -0400

    fix windows compiler path, use toolchain file in Brawlback kit

--- CMake/PPCToolchain.cmake
+++ CMake/PPCToolchain.cmake
@@ -5,10 +5,10 @@ set(CMAKE_CROSSCOMPILING 1)
 #TODO: rename buildSystem directory, sounds too much like SRC_DIR/BuildSystem, which it is not
 # equal to on Linux
 if(WIN32)
-  set(DEVKIT_DIR ${CMAKE_CURRENT_LIST_DIR}/BuildSystem)
+  set(DEVKIT_DIR ${CMAKE_CURRENT_LIST_DIR}/../BuildSystem/Compiler)
 else()
   set(DEVKIT_DIR $ENV{DEVKITPPC})
 endif()
 
 set(CMAKE_CXX_COMPILER ${DEVKIT_DIR}/bin/powerpc-eabi-g++)
-set(CMAKE_C_COMPILER ${DEVKIT_DIR}/bin/powerpc-eabi-gcc)
+set(CMAKE_C_COMPILER "")
