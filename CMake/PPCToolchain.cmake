a578df04d90eb75b37326b0ccfcadbf2fc7e9b27
Elias Reid <47339667+eliasreid@users.noreply.github.com>
2023-01-23 23:38:45 -0400

    remove TODO:

--- CMake/PPCToolchain.cmake
+++ CMake/PPCToolchain.cmake
@@ -2,8 +2,6 @@ set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR ppc)
 set(CMAKE_CROSSCOMPILING 1)
 
-#TODO: rename buildSystem directory, sounds too much like SRC_DIR/BuildSystem, which it is not
-# equal to on Linux
 if(WIN32)
   set(DEVKIT_DIR ${CMAKE_CURRENT_LIST_DIR}/../BuildSystem/Compiler)
   set(CMAKE_CXX_COMPILER ${DEVKIT_DIR}/bin/powerpc-eabi-g++.exe)
