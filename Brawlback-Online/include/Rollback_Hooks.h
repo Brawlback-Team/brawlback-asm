054053e886559c412a71bd3cbdae249673ba8267
Jared M. White <jwhite2292forward@gmail.com>
2025-12-24 20:48:17 -0500

    Clean up code

--- Brawlback-Online/include/Rollback_Hooks.h
+++ Brawlback-Online/include/Rollback_Hooks.h
@@ -1,329 +1,33 @@
 #pragma once
-#include <gf/gf_frame.h>
-#include <gf/gf_task.h>
 #include <gf/gf_pad_system.h>
-#include <gm/gm_global.h>
-#include <gm/gm_global_mode_melee.h>
-#include <gf/gf_game_application.h>
-#include <gf/gf_scene.h>
-#include <sc/sc_melee.h>
-#include <mt/mt_rand.h>
-#include <mu/mu_msg.h>
 #include "ExiStructures.h"
-#include <ip/controls.h>
 #include "exi_packet.h"
 #include <sy_core.h>
-#include <mu/mu_object.h>
-#include <gf/gf_archive.h>
-#include <nw4r/g3d/g3d_scnmdl.h>
-#include <nw4r/g3d/g3d_scngroup.h>
 #if 1
 #define NETPLAY_IMPL
 #define ROLLBACK_IMPL
 #endif
 
-bu32 getCurrentFrame();
-
-void fillOutGameSettings(GameSettings& settings);
-void MergeGameSettingsIntoGame(GameSettings& settings);
-extern bool canRollback;
-extern bu32 frameCounter;
-extern bool shouldTrackAllocs;
-extern bool doDumpList;
-extern bu32 isRollback;
-
 namespace FrameLogic {
     // Variables
-    extern gfTask* task;
-    extern bu32 task_type;
-    extern PlayerFrameData playerFrame;
-    extern gfPadStatus lastLocalInput;
-    extern gfPadStatus inputBuffer;
-    extern gfPadStatus sysPadBuffer;
-    extern bool fixStaleInputs;
-    extern bool shouldSkipTask;
-    extern bu32 r3_value;
-    extern bs32 processFrames;
-    extern bu32 setupPause;
     extern u32 advanceFrames;
     extern u8 port;
-    // Functions
-    void ReduceStickNoise();
-    void FixStaleInputs();
-    void WriteInputsForFrame();
-    void FrameDataLogic();
-    void SendFrameCounterPointerLoc();
-    bool ShouldSkipGfTaskProcess(gfTask* task, bu32 task_type);
-    void GameLoop(void* gameApp, bu32 frames);
 
     // Hooks
-    void initFrameCounter();
-    void updateFrameCounter();
-    void beginningOfMainGameLoop();
-    void beginningOfFrameLoop();
-    void setFrameAdvanceCounter();
-    __attribute__((naked)) void gameProcHook();
-    __attribute__((naked)) void beginningOfFrameLoop2();
-    __attribute__((naked)) void beginningOfFrameLoop3();
-    __attribute__((naked)) void beginningOfFrameLoop4();
-    __attribute__((naked)) void beginningOfFrameLoop5();
-    __attribute__((naked)) void beginningOfFrameLoop6();
-    __attribute__((naked)) void isBreakGameProcLoopHook();
-    __attribute__((naked)) void updateGameFrameAlwaysReturn();
-    __attribute__((naked)) void updateGfSlowManagerAlwaysReturn();
     void beginFrame();
     void endMainLoop();
     void startFrameLoop();
     void getInputs();
     __attribute__((naked)) void startFrameLoop2();
     __attribute__((naked)) void fixFrameLoop();
-    void endFrameLoop();
-    void gfTaskProcessHook();
-    __attribute__((naked)) void gfTaskProcessHook2();
-    __attribute__((naked)) void fixEffects();
-    __attribute__((naked)) void fixEffects2();
-    __attribute__((naked)) void fixEffects3();
-    __attribute__((naked)) void fixEffects4();
-    __attribute__((naked)) void fixEffects5();
-    __attribute__((naked)) void fixEffects6();
-    __attribute__((naked)) void fixEffects7();
-    void setFixStaleInputsTrue();
 }
-namespace FrameAdvance {
-    // Variables
-    extern bu32 framesToAdvance;
-    extern bu32 beforeFramesToAdvance;
-    extern FrameData currentFrameData;
-    extern u32 shouldSkipPadAlarmInstr;
-    extern bool checkPad;
-    // Functions
-    bu32 getFramesToAdvance();
-    void TriggerFastForwardState(bu8 numFramesToFF);
-    void StallOneFrame();
-    void ResetFrameAdvance();
-    void GetInputsForFrame(bu32 frame, FrameData* inputs);
-    void HandlePauseMenuSetupShow();
-    void ProcessGameSimulationFrame(FrameData* inputs);
-    void setFrameAdvanceFromEmu();
-    void getGamePadStatusInjection(gfPadStatus* status, int port, bool isGamePad);
-    void UpdateSync(bu32 frame);
-
-    // Hooks
-    void fixPadInconsistency();
-    void fixVI();
-    void fixGX();
-    void trackEfParticle();
-    void untrackEfParticle();
-    void updateLowHook();
-    void handleFrameAdvanceHook();
-    __attribute__((naked)) void handleFrameAdvanceHook2();
-    void turnOnAllAppropriatePorts();
-    __attribute__((naked)) void moveUpdateSystem();
-}
-
-// TODO: put this in the submodule and pack it
-struct GameReport {
-    f64 damage[MAX_NUM_PLAYERS];
-    s32 stocks[MAX_NUM_PLAYERS];
-    s32 frame_duration;
-};
 
 namespace Util {
-    void printInputs(const BrawlbackPad& pad);
     void printGameInputs(const gfPadStatus& pad);
     void printFrameData(const FrameData& fd);
-    void SyncLog(const BrawlbackPad& pad, bu8 playerIdx);
-    void FixFrameDataEndianness(FrameData* fd);
     BrawlbackPad GamePadToBrawlbackPad(const gfPadStatus& pad);
-    void BrawlbackControlsToGameControls(const BrawlbackControls& brawlbackControls, Controls& controls);
-    BrawlbackControls GameControlsToBrawlbackControls(const Controls& controls);
     void PopulatePlayerFrameData(PlayerFrameData& pfd, bu8 port, bu8 pIdx);
     void InjectBrawlbackPadToPadStatus(gfPadStatus* gamePad, const BrawlbackPad& pad, int port);
-    void SaveState(bu32 currentFrame);
-}
-namespace Match {
-    extern bu32 allocSizeTracker;
-    extern char allocHeapName[30];
-    extern gmSetRule rules;
-    
-    void PopulateGameReport(GameReport& report);
-    void SendGameReport(GameReport& report);
-    void StopGameScMeleeHook();
-    void StartSceneMelee();
-    void ExitSceneMelee();
-    void setRandSeed();
-    void dump_gfMemoryPool_hook();
-    void DumpGfMemoryPoolHook(char** r30_reg_val, bu32 addr_start, bu32 addr_end, bu32 mem_size, u8 id);
-    void ProcessGameAllocation(u8* allocated_addr, bu32 size, char* heap_name);
-    void ProcessGameFree(u8* address, char* heap_name);
-    void alloc_gfMemoryPool_hook();
-    void AllocGfMemoryPoolBeginHook(char** internal_heap_data, bu32 size, bu32 alignment);
-    void allocGfMemoryPoolEndHook();
-    void free_gfMemoryPool_hook();
-    void FreeGfMemoryPoolHook(char** internal_heap_data, u8* address);
-}
-
-namespace Netplay {
-    // Variables
-    extern GameSettings gameSettings;
-    extern const bu8 localPlayerIdxInvalid;
-    extern bu8 localPlayerIdx;
-    extern bool isInMatch;
-    extern bool foundMatch;
-    extern bool isInTrainingRoom;
-    // Functions
-    static void* StartMatching(void*);
-    bool CheckIsMatched();
-    void EndMatch();
-
-    bool IsInMatch();
-    void SetIsInMatch(bool isMatch);
-
-    int getLocalPlayerIdx();
-    GameSettings& getGameSettings();
-}
-
-namespace NetMatchMakingStatus {
-    const bu32 LinkDown = 0x0;
-    const bu32 LinkUp = 0x1;
-    const bu32 DWCReady = 0x2;
-    const bu32 WaitLogin = 0x3;
-    const bu32 Download = 0x4;
-    const bu32 Online = 0x5;
-    const bu32 MatchingStart = 0x6;
-    const bu32 Matching = 0x7;
-    const bu32 MatchingWaitShare = 0x8;
-    const bu32 P2P = 0x9;
-    const bu32 DisconnectAll = 0xa;
-    const bu32 WaitShutdown = 0xb;
-    const bu32 Error = 0xc;
-}
-
-namespace Scene {
-    const bu32 None = 0x0;
-    const bu32 MemoryChange = 0x1;
-    const bu32 Idle = 0x2;
-    const bu32 InitialChange = 0x3;
-    const bu32 SelectCharacter = 0x4;
-    const bu32 Unk5 = 0x5;
-    const bu32 SelStage = 0x6;
-    const bu32 Unk7 = 0x7;
-    const bu32 Unk8 = 0x8;
-    const bu32 Melee = 0x9;
-    const bu32 Unk10 = 0xa;
-    const bu32 Unk11 = 0xb;
-    const bu32 VsResult = 0xc;
-    const bu32 Unk13 = 0xd;
-    const bu32 PrizeCheck = 0xe;
-    const bu32 Unk15 =	0xf;
-    const bu32 MenuMain = 0x10;
-}
-
-namespace NetMenu {
-    // Variables
-    extern bool skipToCSS;
-    extern bool setRules;
-    extern bool onQuickplayMenus;
-    extern int register4;
-    extern MuMsg* message;
-    extern MuObject* pauseMenu[4];
-    extern gfArchive* pauseArchive;
-    extern ScnGroup* pauseModel;
-    extern nw4r::g3d::ResFileData* menuData;
-    extern MuMsg* pauseMusicMessage;
-    extern bu8 numPauseModels;
-    extern bu32 showPauseMenu;
-    extern bu16 lastFrameShowPauseMenu;
-    // Functions
-    void ChangeGfSceneField(bu32 scene);
-    void ChangeStruct3Scenes(bu8* structure, bu32 scene, bu32 nextScene);
-    void ChangeStruct3Scenes(bu8* structure, bu32 scene);
-    void BootToScMelee();
-    void MatchmakingText();
-    // Hooks
-    __attribute__((naked)) void setToLoggedIn();
-    __attribute__((naked)) void setToLoggedIn2();
-    __attribute__((naked)) void disableMiiRender();
-    __attribute__((naked)) void disableMatchmakingError();
-    void connectToAnybodyAsyncHook();
-    __attribute__((naked)) void connectToAnybodyAsyncHook2();
-    __attribute__((naked)) void disableCreateCounterOnCSS();
-    __attribute__((naked)) void turnOffCSSTimer();
-    __attribute__((naked)) void disableCreateCounterOnSSS();
-    __attribute__((naked)) void turnOffSSSTimer();
-    __attribute__((naked)) void disableGetNetworkErrorOnCSS();
-    __attribute__((naked)) void disableGetNetworkErrorOnSSS();
-    __attribute__((naked)) void forceFriendCode();
-    __attribute__((naked)) void forceConnection();
-    void startMatchingCallback();
-    void setNextAnyOkirakuTop();
-    void setNextAnyOkirakuCaseFive();
-    void netThreadTaskOverride();
-    __attribute__((naked)) void BBisCompleteMeleeSettingAllMember();
-    __attribute__((naked)) void BBisWifiPreloadCharacter();
-    __attribute__((naked)) void BBisCompleteCloseMatchingAllNode();
-    __attribute__((naked)) void BBisPlayerAssignReceived();
-    __attribute__((naked)) void netThreadTaskOverride2();
-    __attribute__((naked)) void startMatchingCallback2();
-    __attribute__((naked)) void BBSkipgmInitGlobalMelee();
-    __attribute__((naked)) void BBSkipgmInitGlobalMelee2();
-    void RemoveDisconnectPanel();
-    __attribute__((naked)) void RemoveDisconnectPanel2();
-    void RemoveDisconnectPanel3();
-    void BBsetNextAnyOriakuBootScMelee();
-    __attribute__((naked)) void BBSkipRandomRulesetChange();
-    void BBSetGameModeBitCorrectly();
-    __attribute__((naked)) void BBSetGameModeBitCorrectly2();
-    void BBSetupCharacters();
-    void BBSetupNetMelee();
-    __attribute__((naked)) void ExitWifiCSSReturnsToDirectOrQuickplayScreen();
-    void BBBootTosqNetAnyOkiraku();
-    void ExitWifiCSSReturnsToDirectOrQuickplayScreen2();;
-    void SkipDirectlyToCSS();
-    void SkipDirectlyToTrainingRoom();
-    void GetRulesFromCSSBoot();
-    void SetRulesFromCSSBoot();
-    void ReplaceTrainingRoomText();
-    void StartReplacementPauseMenu();
-    __attribute__((naked)) void RenderReplacementPauseMenu();
-    void OpenReplacementPauseMenu();
-    void CloseReplacementPauseMenu();
-    void OverridePauseSetting();
-    __attribute__((naked)) void OverridePauseSetting2();
-}
-
-namespace NetReport {
-    // Variables
-
-    // Functions
-    void netReportHookFunc(char* str);
-
-    // Hooks
-    __attribute__((naked)) void netReportHook();
-    __attribute__((naked)) void netReportHook2();
-    __attribute__((naked)) void netReportHook3();
-    __attribute__((naked)) void netReportHook4();
-    __attribute__((naked)) void netMinReportHook();
-}
-
-extern u8 defaultGmGlobalModeMelee[0x320];
-namespace GMMelee {
-    // Variables
-    extern bool isMatchChoicesPopulated;
-    extern bu8 charChoices[MAX_NUM_PLAYERS];
-    extern s8 fileIndexChoices[MAX_NUM_PLAYERS];
-    extern bool rumbleChoices[MAX_NUM_PLAYERS];
-    extern s8 costumeChoices[MAX_NUM_PLAYERS];
-    extern  BrawlbackControls controlsChoices[MAX_NUM_PLAYERS];
-    extern int stageChoice;
-    #define STAGE_ID_IDX 27
-
-    // Functions
-    void PopulateMatchSettings(bu8 chars[4], s8 costumes[4], s8 fileIndices[4], bool rumble[4], BrawlbackControls controls[4], int stageID);
-    void ResetMatchChoicesPopulated();
-
-    // Hooks
-    void postSetupMelee();
 }
 
 namespace RollbackHooks {
